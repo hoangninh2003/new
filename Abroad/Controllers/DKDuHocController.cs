f9488c5f829704badaf74e2ed4ffb04ef24172f7
hoangninh2003 <128906612+hoangninh2003@users.noreply.github.com>
2023-07-26 08:52:29 +0700

    Initial commit

---
+++ Abroad/Controllers/DKDuHocController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Abroad.Controllers
+{
+    public class DKDuHocController : Controller
+    {
+        // GET: DKDuHoc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Abroad/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Abroad.MvcApplication" Language="C#" %>
